5bd1fc0b95c4f9757811f3c93ef288f039ee52a2
Ligandlly <liu_lingyuan@icloud.com>
2023-04-26 16:04:39 +0800

    add login view group

--- app/src/main/java/com/example/fit5046assign3/MainActivity.java
+++ app/src/main/java/com/example/fit5046assign3/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.fit5046assign3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText userName = findViewById(R.id.user_name_edittext);
+        EditText password = findViewById(R.id.password_edittext);
+        Button login = findViewById(R.id.login_button);
+
+        login.setOnClickListener(v -> {
+            String usernameText = userName.getText().toString();
+            String passwordText = password.getText().toString();
+            Log.d("DEBUG", "username: " + usernameText + "password: " + passwordText);
+        });
     }
 }
